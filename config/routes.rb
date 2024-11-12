5df6720ab7e2237394c5a6cd716847efad807f0b
Kyle Delaney <khdelaney99@gmail.com>
2024-11-12 13:17:10 -0500

    add routing for api

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :subscriptions, except: [:destroy, :create]
+    end
+  end
 end
