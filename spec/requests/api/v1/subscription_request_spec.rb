e10d6c8e68eda1cd30996ef63e24ad010bbb92ff
Kyle Delaney <khdelaney99@gmail.com>
2024-11-12 15:56:28 -0500

    add structure to subscription request spec

---
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Pokemon Endpoints' do
+    describe 'Show all tea subscriptions' do
+        describe 'HAPPY paths' do
+
+        end
+
+        describe 'SAD paths' do
+
+        end
+    end
+
+    describe 'Show single tea subscriptions' do
+        describe 'HAPPY paths' do
+
+        end
+
+        describe 'SAD paths' do
+
+        end
+    end
+
+    describe 'Update single tea subscriptions' do
+        describe 'HAPPY paths' do
+
+        end
+
+        describe 'SAD paths' do
+            
+        end
+    end
+end
