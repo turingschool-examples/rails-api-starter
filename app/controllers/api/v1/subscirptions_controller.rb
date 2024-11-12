8936525dc4183a96d35a62770d6f4e513ea350bf
Jim Macur <jim.macur@gmail.com>
2024-11-12 10:34:18 -0700

    refactor: refactors subscription controller with helper methods

--- app/controllers/api/v1/subscirptions_controller.rb
+++ app/controllers/api/v1/subscirptions_controller.rb
@@ -5,47 +5,53 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def show
-    subscription = Subscription.find(params[:id])
-      render json: {
-        id: subscription.id,
-        title: subscription.title,
-        price: subscription.price,
-        status: subscription.status,
-        frequency: subscription.frequency,
-        customer: {
-          first_name: subscription.customer.first_name,
-          last_name: subscription.customer.last_name,
-          email: subscription.customer.email,
-          address: subscription.customer.address
-        },
-        teas: subscription.teas.map { |tea|  { title: tea.title, description: tea.description } }
-      }
-  rescue ActiveRecord::RecordNotFound
-    render json: { message: 'Subscription not found' }, status: :not_found
+    subscription = find_subscription
+    render_subscription(subscription)
   end
 
   def update
-    subscription = Subscription.find(params[:id])
+    subscription = find_subscription
     
     if subscription.update(status: 'canceled')
-      render json: {
-        id: subscription.id,
-        title: subscription.title,
-        price: subscription.price,
-        status: subscription.status,
-        frequency: subscription.frequency,
-        customer: {
-          first_name: subscription.customer.first_name,
-          last_name: subscription.customer.last_name,
-          email: subscription.customer.email,
-          address: subscription.customer.address
-        },
-        teas: subscription.teas.map { |tea| { title: tea.title, description: tea.description } }
-      }, status: :ok
-    else
-      render json: { message: 'Failed to update subscription' }, status: :unprocessable_entity
+      render_subscription(subscription, :ok)
+    else  
+      render json: {message: "Failed to update subscription" }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def render_subscription(subscription, status = :ok)
+    render json: {
+      id: subscription.id,
+      title: subscription.title,
+      price: subscription.price,
+      status: subscription.status,
+      frequency: subscription.frequency,
+      customer: customer_details(subscription),
+      teas: tea_details(subscription)
+    }, status: status
+  end
+
+  def customer_details(subscription)
+    {
+      first_name: subscription.customer.first_name,
+      last_name: subscription.customer.last_name,
+      email: subscription.customer.email,
+      address: subscription.customer.address
+    }
+  end
+
+  def tea_details(subscription)
+    subscription.teas.map do |tea|
+      { title: tea.title, description: tea.description }
+    end
+  end
+
+  def find_subscription
+    Subscription.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render json: { message: 'Subscription not found' }, status: :not_found
   end
 end
+
